f2464f25cee4fe53744d72bd3180c12fd3a0982f
Daniel <Dannyking07@outlook.es>
2024-03-02 01:08:15 -0500

    [feat app.py -> root]

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from src.routers.home import home
+from src.routers.auth import auth
+# from src.routers.user import user
+# from src.routers.tours import tours
+from src.routers.reserves import reserve
+from config import DATABASE_CONNECTION_URI, Config
+from src.utils.db import db
+
+
+def app_create():
+    app = Flask(__name__)
+
+    print(DATABASE_CONNECTION_URI)
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_CONNECTION_URI
+    app.config.from_object(Config)
+
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+
+    app.register_blueprint(home)
+    app.register_blueprint(auth)
+    # app.register_blueprint(user)
+    # app.register_blueprint(tours)
+    app.register_blueprint(reserve)
+
+    return app
