abfda2fb9a17a36c50d2beba1e4a7a5de5619104
Daniel <Dannyking07@outlook.es>
2024-03-03 10:34:36 -0500

    update index.py -> root

--- index.py
+++ index.py
@@ -1,8 +1,11 @@
 from app import app_create, db
+from src.utils.rolecreate import create_roles, create_admin_user
 
 app = app_create()
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+        create_roles()
+        create_admin_user()
     app.run(debug=True)
