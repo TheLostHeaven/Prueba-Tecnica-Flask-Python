a49d743d220de08c37b61406fe901446496af32c
Daniel <Dannyking07@outlook.es>
2024-03-03 04:16:20 -0500

    feat [config enviroments]

--- config.py
+++ config.py
@@ -11,6 +11,7 @@ user = os.environ["MYSQL_USER"]
 password = os.environ["MYSQL_PASSWORD"]
 host = os.environ["MYSQL_HOST"]
 database = os.environ["MYSQL_DATABASE"]
+jwtKey = os.environ["JTW_KEY"]
 
 DATABASE_CONNECTION_URI = f'mysql://{user}:{password}@{host}/{database}'
 print(DATABASE_CONNECTION_URI)
