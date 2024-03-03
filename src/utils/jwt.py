929343083a78793d2266306613be2f3ca2be9416
Daniel <Dannyking07@outlook.es>
2024-03-03 17:48:48 -0500

    update [JWT -> utils]

--- src/utils/jwt.py
+++ src/utils/jwt.py
@@ -2,10 +2,12 @@ import jwt
 from datetime import datetime, timedelta
 from config import jwtKey
 
-def generate_jwt(user_id):
+def generate_jwt(user_id, user):
     payload = {
         'user_id': user_id,
-        'exp': datetime.utcnow() + timedelta(days=1)
+        'username': user.username,
+        'role_id': user.role_id,
+        'exp': datetime.utcnow() + timedelta(minutes=1)
     }
     secret_key = jwtKey
     token = jwt.encode(payload, secret_key, algorithm='HS256')
