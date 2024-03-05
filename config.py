80eccf8746c35fbb27dec17af1a2167c79d3ef10
Daniel <Dannyking07@outlook.es>
2024-03-05 14:15:23 -0500

    update [config -> root]environmets testing

--- config.py
+++ config.py
@@ -19,5 +19,11 @@ email=os.getenv("ADMIN_EMAIL"),
 password_admin=os.getenv("ADMIN_PASSWORD"),
 role_id=os.getenv("ADMIN_ROLE_ID")
 
+user_test = os.environ["MYSQL_USER_TEST"]
+password_test = os.environ["MYSQL_PASSWORD_TEST"]
+host_test = os.environ["MYSQL_HOST_TEST"]
+database_test = os.environ["MYSQL_DATABASE_TEST"]
+
 DATABASE_CONNECTION_URI = f'mysql://{user}:{password}@{host}/{database}'
-print(DATABASE_CONNECTION_URI)
+
+DATABASE_CONNECTION_URI_TEST = f'mysql://{user_test}:{password_test}@{host_test}/{database_test}'
