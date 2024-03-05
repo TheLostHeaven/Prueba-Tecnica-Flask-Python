4081d52c9f26159deaeb52a0d8d3094a3dcf86f3
Daniel <Dannyking07@outlook.es>
2024-03-05 18:31:56 -0500

    update [test_auth -> test]

--- src/test/test_auth.py
+++ src/test/test_auth.py
@@ -19,11 +19,3 @@ def test_login(client):
     assert response.status_code == 200
     assert json_data['token'] is not None
 
-
-# def test_register(client):
-#     data = {'email':'correo@gmail2.com', 'fullname':'nombreee2', 'password': 'contrasena_test', 'username': 'usuario_test2'}
-#     print("Datos enviados:", data) 
-#     response = client.post('/register', json=data)
-#     json_data = response.get_json()
-
-
