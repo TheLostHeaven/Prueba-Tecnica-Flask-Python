7c5c817cacd1485ad95a59b5c75bdc8ac5717d38
Daniel <Dannyking07@outlook.es>
2024-03-05 17:46:53 -0500

    update [conftest -> test]

--- src/test/conftest.py
+++ src/test/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from src.utils.db import db
 from app import app_create
+import time
 from src.utils.rolecreate import create_roles
 from sqlalchemy import text
 from config import DATABASE_CONNECTION_URI_TEST
@@ -15,6 +16,7 @@ def app():
         create_roles()
         yield app
         db.session.remove()
+        time.sleep(10)
         db.drop_all()
 
 @pytest.fixture
@@ -24,6 +26,7 @@ def client(app):
 def cleanup_database(app):
     print("Limpiando la base de datos...")
     with app.app_context():
+        time.sleep(10)
         with db.session.begin():
             db.session.execute(text("SET foreign_key_checks = 0"))
             db.session.execute(text("DELETE FROM user"))
